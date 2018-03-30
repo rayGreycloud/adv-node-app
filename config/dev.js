9621acb3b57d71327fa133054fb57256cee2d4f4
rayGreycloud <rayGreycloud@gmail.com>
2018-03-29 20:10:14 -0700

    Update dev config

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
   googleClientID:     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: `KnH-rZC23z4fr2CN4ISK4srN`,
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
+  mongoURI: 'mongodb://admin:admin@ds031915.mlab.com:31915/blog_dev',
   cookieKey: '123123123'
 };
