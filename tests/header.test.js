04a576c48a89bbbde7bf0ce7d6270d4802ef8d33
rayGreycloud <rayGreycloud@gmail.com>
2018-04-01 14:57:58 -0700

    Refactor using custom page class

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,24 +1,17 @@
-const puppeteer = require('puppeteer');
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
+const Page = require('./helpers/page');
 
-let brower;
 let page;
 
 beforeEach(async () => {
-  // Create browser
-  browser = await puppeteer.launch({
-    headless: false
-  });
   // Open tab
-  page = await browser.newPage();
+  page = await Page.build();
   // Navigate to app 
   await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
   // Close browser
-  await browser.close();
+  await page.close();
 });
 
 test('Should have correct text in header', async () => {
@@ -38,23 +31,8 @@ test('Should start oauth flow when login clicked', async () => {
 });
 
 test('Should show logout button when signed in', async () => {
-  // Get user from user factory 
-  const user = await userFactory();
-  // Pass in user and get session 
-  const { session, sig } = sessionFactory(user);
-  // Set cookies 
-  await page.setCookie({
-    name: 'session',
-    value: session
-  });
-  await page.setCookie({
-    name: 'session.sig',
-    value: sig
-  });
-  // Refresh page 
-  await page.goto('localhost:3000');
-  // 
-  await page.waitFor('a[href="/auth/logout"]');
+  await page.login();
+  
   // Get logout button
   const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
   
