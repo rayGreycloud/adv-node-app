2a8f4fd35c615068e19488fb35a7d6c279214614
rayGreycloud <rayGreycloud@gmail.com>
2018-04-01 14:56:52 -0700

    Refactor puppeteer and login logic into custom class with proxy

---
+++ tests/helpers/page.js
@@ -0,0 +1,46 @@
+const puppeteer = require('puppeteer');
+const sessionFactory = require('.././factories/sessionFactory');
+const userFactory = require('.././factories/userFactory');
+
+class CustomPage {
+  static async build() {
+    const browser = await puppeteer.launch({
+      headless: false
+    });
+    
+    const page = await browser.newPage();
+    const customPage = new CustomPage(page);
+    
+    return new Proxy(customPage, {
+      get: function (target, property) {
+        return customPage[property] || browser[property] || page[property];
+      }
+    });
+  }
+  
+  constructor(page) {
+    this.page = page;
+  }
+  
+  async login() {
+    // Get user from user factory 
+    const user = await userFactory();
+    // Pass in user and get session 
+    const { session, sig } = sessionFactory(user);
+    // Set cookies 
+    await this.page.setCookie({
+      name: 'session',
+      value: session
+    });
+    await this.page.setCookie({
+      name: 'session.sig',
+      value: sig
+    });
+    // Refresh page 
+    await this.page.goto('localhost:3000');
+    // Wait until element appears
+    await this.page.waitFor('a[href="/auth/logout"]');    
+  }
+}
+
+module.exports = CustomPage;
