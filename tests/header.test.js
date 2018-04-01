04c8dff48fbee5301998ecfc14aa99c831ffe661
rayGreycloud <rayGreycloud@gmail.com>
2018-04-01 08:25:20 -0700

    Test oauth flow started on login click

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,12 +1,38 @@
 const puppeteer = require('puppeteer');
+let brower;
+let page;
 
-// Testing 
-test('Should launch a browser', async () => {
-  const browser = await puppeteer.launch({
+beforeEach(async () => {
+  // Create browser
+  browser = await puppeteer.launch({
     headless: false
   });
-  const page = await browser.newPage();
-  
-  
-  
+  // Open tab
+  page = await browser.newPage();
+  // Navigate to app 
+  await page.goto('localhost:3000');
 });
+
+afterEach(async () => {
+  // Close browser
+  await browser.close();
+});
+
+test('Should have correct text in header', async () => {
+  // Get element text 
+  const text  = await page.$eval('a.brand-logo', el => el.innerHTML);
+  // Check value 
+  expect(text).toEqual('Blogster');
+});
+
+test('Should start oauth flow when login clicked', async () => {
+  // 'Click' on login 
+  await page.click('.right a');
+  // Get page url 
+  const url = await page.url();
+  // Check url 
+  expect(url).toMatch(/accounts\.google\.com/);
+});
+
+
+
