995b4ca20f347a2f4d075728d0ab220bbc12a9a7
rayGreycloud <rayGreycloud@gmail.com>
2018-04-01 08:26:28 -0700

    Test logout button shown when signed in

--- tests/header.test.js
+++ tests/header.test.js
@@ -34,5 +34,38 @@ test('Should start oauth flow when login clicked', async () => {
   expect(url).toMatch(/accounts\.google\.com/);
 });
 
+test.only('Should show logout button when signed in', async () => {
+  // db user id
+  const id = "5abd55ccba969806545e328a";
+  // Create buffer instance
+  const Buffer = require('safe-buffer').Buffer;
+  // Create session object
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+  // Create session string 
+  const sessionString = Buffer.from(
+    JSON.stringify(sessionObject)
+  ).toString('base64');
 
+  // Create signature 
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign(`session=${sessionString}`);
+  // Set cookies 
+  await page.setCookie({
+    name: 'session',
+    value: sessionString
+  });
+  await page.setCookie({
+    name: 'session.sig',
+    value: sig
+  });
+  // Refresh page 
+  await page.goto('localhost:3000');
+  
+});
 
