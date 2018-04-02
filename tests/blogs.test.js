88bd1d003a518c5788125b0449f0cf6ef752c2c0
rayGreycloud <rayGreycloud@gmail.com>
2018-04-02 10:09:28 -0700

    Set up test suite for blogs; test proper navigation on button click when logged in

---
+++ tests/blogs.test.js
@@ -0,0 +1,22 @@
+const Page = require('./helpers/page');
+
+let page;
+
+beforeEach(async () => {
+  page = await Page.build();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await page.close();
+});
+
+test('Should navigate to new blog form on button click when logged in', async () => {
+  await page.login();
+  await page.click('a.btn-floating');
+  
+  const label = await page.getContentsOf('form label');
+  
+  expect(label).toEqual('Blog Title');
+});
+
