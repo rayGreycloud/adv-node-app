fe3e6e4b28ca8eb7dca4ee28eb9b28973e92ca7c
rayGreycloud <rayGreycloud@gmail.com>
2018-04-03 06:16:10 -0700

    Refactor header tests with describe block

--- tests/header.test.js
+++ tests/header.test.js
@@ -2,40 +2,44 @@ const Page = require('./helpers/page');
 
 let page;
 
-beforeEach(async () => {
-  // Open tab
-  page = await Page.build();
-  // Navigate to app 
-  await page.goto('localhost:3000');
+describe('The Header', async () => {
+  beforeEach(async () => {
+    // Open tab
+    page = await Page.build();
+    // Navigate to app 
+    await page.goto('localhost:3000');
+  });
+
+  afterEach(async () => {
+    // Close browser
+    await page.close();
+  });
+
+  test('Should have show correct text', async () => {
+    // Get element text 
+    const text  = await page.getContentsOf('a.brand-logo');
+    // Check value 
+    expect(text).toEqual('Blogster');
+  });
+
+  test('Should start oauth flow when login button clicked', async () => {
+    // 'Click' on login 
+    await page.click('.right a');
+    // Get page url 
+    const url = await page.url();
+    // Check url 
+    expect(url).toMatch(/accounts\.google\.com/);
+  });
+
+  test('Should show logout button when signed in', async () => {
+    await page.login();
+    
+    // Get logout button
+    const text = await page.getContentsOf('a[href="/auth/logout"]');
+    
+    expect(text).toEqual('Logout');
+  });
+
 });
 
-afterEach(async () => {
-  // Close browser
-  await page.close();
-});
-
-test('Should have correct text in header', async () => {
-  // Get element text 
-  const text  = await page.getContentsOf('a.brand-logo');
-  // Check value 
-  expect(text).toEqual('Blogster');
-});
-
-test('Should start oauth flow when login clicked', async () => {
-  // 'Click' on login 
-  await page.click('.right a');
-  // Get page url 
-  const url = await page.url();
-  // Check url 
-  expect(url).toMatch(/accounts\.google\.com/);
-});
-
-test('Should show logout button when signed in', async () => {
-  await page.login();
-  
-  // Get logout button
-  const text = await page.getContentsOf('a[href="/auth/logout"]');
-  
-  expect(text).toEqual('Logout');
-});
 
