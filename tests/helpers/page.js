4cca5900fd973ca7ec5f0fb1b1edc11691752c02
rayGreycloud <rayGreycloud@gmail.com>
2018-04-03 14:19:57 -0700

    Add get and post helper methods using fetch

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -45,6 +45,31 @@ class CustomPage {
   async getContentsOf(selector) {
     return this.page.$eval(selector, el => el.innerHTML);    
   }
+  
+  async get(path) {
+    return this.page.evaluate((_path) => {
+      return fetch(_path, {
+        method: 'GET',
+        credentials: 'same-origin',
+        headers: {
+          'Content-type': 'application/json'
+        }
+      }).then(res => res.json());
+    }, path);    
+  }
+  
+  async post(path, data) {
+    return this.page.evaluate((_path, _data) => {
+      return fetch(_path, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(_data)
+      }).then(res => res.json());
+    }, path, data);
+  }
 }
 
 module.exports = CustomPage;
