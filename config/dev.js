7801da561c5fca50fd03ae7edcc365e640092cde
rayGreycloud <rayGreycloud@gmail.com>
2018-04-04 08:26:17 -0700

    Add redis url to dev key because no longer hard-coded in app

--- config/dev.js
+++ config/dev.js
@@ -2,5 +2,6 @@ module.exports = {
   googleClientID:     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: `KnH-rZC23z4fr2CN4ISK4srN`,
   mongoURI: 'mongodb://admin:admin@ds031915.mlab.com:31915/blog_dev',
-  cookieKey: '123123123'
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'   
 };
